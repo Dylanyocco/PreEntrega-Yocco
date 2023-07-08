c3228f927d3830132e261c0aac46efad3045738c
Dylan Alexis Yocco <dylanalexisyocco@gmail.com>
2023-07-08 16:19:35 -0300

    1er carrito

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const botonesAgregarAlCarrito = document.getElementsByClassName('boton-item');
 const carrito = [];
-const carritoList = document.getElementById('carrito-list');
+const carritoList = document.querySelector('#carrito-list');
 
 
 for (let i = 0; i < botonesAgregarAlCarrito.length; i++) {
@@ -13,12 +13,29 @@ const button = event.target;
 const item = button.parentElement;
 const titulo = item.querySelector('.titulo-item').textContent;
 const precio = item.querySelector('.precio-item').textContent;
+const imagen = item.querySelector('.img-item').src;
 
 const producto = {
     titulo: titulo,
-    precio: precio
+    precio: precio,
+    imagen: imagen,
 };
 
 carrito.push(producto);
-console.log('Producto agregado al carrito:', producto);
+console.log( carrito);
 }
+
+carritoList.addEventListener("click", () => {
+    carritoList.innerHTML = '';
+    carrito.forEach((el) => {
+    const item = document.createElement("div");
+    item.classList.add("item");
+    item.innerHTML = `
+        <div class="item">
+        <img class="img-item" src="${el.imagen}">
+        <h2 class="titulo-item">${el.titulo}</h2>
+        <p class="precio-item">${el.precio}</p>
+        </div>`;
+    carritoList.appendChild(item);
+    });
+});
